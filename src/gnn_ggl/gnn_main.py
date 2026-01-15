19befe962394550e047d60d02c3507920bdb4615
C3L0 <96005279+C3L0@users.noreply.github.com>
2026-01-15 20:32:58 +0100

    add evalution - clean branch

--- src/gnn_ggl/gnn_main.py
+++ src/gnn_ggl/gnn_main.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 import torch
 import torch.nn.functional as F
+import os
 from torch.utils.data import DataLoader
 
 from src.gnn_ggl.data import CantileverMeshDataset
 from src.gnn_ggl.model import SolidGNN
-
+from src.gnn_ggl.evaluation import visualize_gnn_results 
 
 def run_simulation():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -38,22 +39,30 @@ def run_simulation():
         if epoch % 20 == 0:
             print(f"Epoch {epoch} | Loss: {epoch_loss / len(loader):.6f}")
 
-    # Simple Visual Eval
-    model.eval()
-    with torch.no_grad():
-        test_x, test_y = dataset[0]
-        pred_y = model(test_x.unsqueeze(0).to(device)).squeeze(0).cpu().numpy()
+    visualize_gnn_results(model, dataset, device, sample_idx=5)
 
-    plt.scatter(dataset.coords[:, 0], dataset.coords[:, 1], c="gray", alpha=0.2)
-    plt.scatter(
-        dataset.coords[:, 0] + pred_y[:, 0] * 5,
-        dataset.coords[:, 1] + pred_y[:, 1] * 5,
-        c="red",
-        label="GNN Pred",
-    )
-    plt.legend()
-    plt.title("GNN Result (Amplified 5x)")
-    plt.show()
+     # Save the model
+    if not os.path.exists("src/gnn_ggl/model"):
+        os.makedirs("src/gnn_ggl/model")
+    torch.save(model.state_state_dict(), "src/gnn_ggl/model/gnn_v2.pth")
+    print("Training complete and model saved.")
+
+    # Simple Visual Eval
+    # model.eval()
+    # with torch.no_grad():
+    #     test_x, test_y = dataset[0]
+    #     pred_y = model(test_x.unsqueeze(0).to(device)).squeeze(0).cpu().numpy()
+
+    # plt.scatter(dataset.coords[:, 0], dataset.coords[:, 1], c="gray", alpha=0.2)
+    # plt.scatter(
+    #     dataset.coords[:, 0] + pred_y[:, 0] * 5,
+    #     dataset.coords[:, 1] + pred_y[:, 1] * 5,
+    #     c="red",
+    #     label="GNN Pred",
+    # )
+    # plt.legend()
+    # plt.title("GNN Result (Amplified 5x)")
+    # plt.show()
 
 
 if __name__ == "__main__":
